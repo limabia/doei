ca996944663dcdb999637fc43271d13c6252de15
Marcelo Ferreira Rodrigues <mrodrigues.eng@hotmail.com>
2020-10-11 12:24:42 -0300

    Complemento

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,23 +1,12 @@
 class Usuario < ActiveRecord::Base
     has_secure_password
-    validates :nome, presence:  true
-    validates :password, length: { minimum: 6 }
+    validates :nome, presence:  false
+    validates :password, presen length: { minimum: 6 }
+    validates :password_confirmation, presence: true
     validates :email,
-        presence: true,
+        presence: { message: "não pode ser vazio." },
         uniqueness: true,
         format: {
         with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-        }
-    validates :cpf,
-        presence: true,
-        uniqueness: true,
-        format: {
-        with: /\d{3}\.\d{3}\.\d{3}\-\d{2}\z/i
-        } 
-    validate :age_greater_than_18?
-    def age_greater_than_18?
-      if dataNascimento.present? && dataNascimento > Date.today - 18.years
-        errors.add(:dataNascimento, "Deve ser maior de 18 anos")
-      end
-    end
+        }    
 end
