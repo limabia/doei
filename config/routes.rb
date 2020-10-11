a841635f7424c3946946b7672ea40f7818447f36
Mauricio Tavares Ozaki <ozaki@versatushpc.com.br>
2020-10-11 00:07:02 -0300

    Exibindo a busca sem login.

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'sair', to: 'sessoes#destroy'
   get 'cadastrar', to: 'usuarios#new'
   get 'produtos/cadastrar', to: 'produtos#new'
-  get 'produtos/busca', to: 'produtos#busca', as: :busca_produto
+  get 'sessoes/busca', to: 'sessoes#busca', as: :buscar_produto
   get 'produtos', to: 'produtos#index'
   get 'authorized', to: 'sessoes#page_requires_login'
   get '/', to:"home#index"
