30c7ad98c071b1f057e66879cbb09297ce33a8bd
Bianca Lima <bianca.lima.1504@gmail.com>
2020-10-17 22:09:25 -0300

    ajustando o redirecionamento do usuario partindo do index e adicionando alguns links para melhorar a usabilidade do site

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,29 +1,15 @@
 class ProdutosController < ApplicationController
-    def index   
+    def index  
       @produtos = Produto.order :nome
     end
 
     def new       
       @produto = Produto.new  
     end
+    
     def create
       @produto = Produto.new(produto_params) 
-      # TODO pegar da session o usuario
-      if not Usuario.exists?
-        u = Usuario.new
-        u.nome = 'user teste'
-        u.email = 'user@teste.com'
-        u.cpf = '647.249.980-24'
-        u.dataNascimento = '2000-10-10'
-        u.cep = '03590080'
-        u.telefone = '11987678976'
-        u.password = 'secret123456'
-        u.save
-      else 
-        u = Usuario.first
-      end
-      # retirar esse trecho quando tiver criação de usuários e login funcionando
-      @produto.usuario_id = u.id
+      @produto.usuario_id = current_user.id
       if @produto.save
         redirect_to produtos_path
       else
