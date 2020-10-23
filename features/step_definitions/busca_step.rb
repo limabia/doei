8c171d5105242f5b8cc8d3db079aff9011996e13
Renato Takakura <renatotkr@gmail.com>
2020-10-23 15:54:51 -0300

    finalizando feature de busca avancada

---
+++ features/step_definitions/busca_step.rb
@@ -0,0 +1,16 @@
+db = SQLite3::Database.open('db/test.sqlite3')
+db.results_as_hash = true
+user = db.execute("Select id from 'usuarios' limit 1")
+db.execute("DELETE FROM 'produtos' WHERE nome = 'camiseta teste'")
+db.execute("DELETE FROM 'produtos' WHERE categoria = 'roupa masculina'")
+db.execute("INSERT INTO 'produtos' (nome, categoria, tamanho, situacao, created_at, updated_at,usuario_id) VALUES ('camiseta teste', 'roupa','P','Usado', '2020-10-10', '2020-10-10', #{user[0][0]})")
+db.execute("INSERT INTO 'produtos' (nome, categoria, tamanho, situacao, created_at, updated_at,usuario_id) VALUES ('camiseta', 'roupa masculina','M','Novo', '2020-10-10', '2020-10-10', #{user[0][0]})")
+
+
+Dado('que estou na tela de busca') do
+    visit '/sessoes/busca' 
+end
+
+Dado('deverei ver o texto {string}') do |string|
+    expect(page).to have_content(string)
+end
