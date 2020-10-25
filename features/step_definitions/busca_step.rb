9369aec003ee78b7b7f4140eca299ecca2e09f3c
Renato Takakura <renatotkr@gmail.com>
2020-10-25 19:15:53 -0300

    fix test

--- features/step_definitions/busca_step.rb
+++ features/step_definitions/busca_step.rb
@@ -1,5 +1,9 @@
+require 'sqlite3'
+pass =  BCrypt::Password.create('mewtwo')
 db = SQLite3::Database.open('db/test.sqlite3')
 db.results_as_hash = true
+db.execute("DELETE FROM 'usuarios' WHERE email = 'produto@gmail.com'")
+db.execute("INSERT INTO 'usuarios' (nome, password_digest, email, ativo, created_at, updated_at) VALUES ('teste inativo','#{pass}', 'produto@gmail.com',0, 121212, 121212)")
 user = db.execute("Select id from 'usuarios' limit 1")
 db.execute("DELETE FROM 'produtos' WHERE nome = 'camiseta teste'")
 db.execute("DELETE FROM 'produtos' WHERE categoria = 'roupa masculina'")
