6c4cc3b6bda8fdbc7ba620451861f0fd45a61f84
Mauricio Tavares Ozaki <ozaki@versatushpc.com.br>
2020-10-30 20:03:20 -0300

    Testes de unidade.

--- spec/controllers/produtos_controller_spec.rb
+++ spec/controllers/produtos_controller_spec.rb
@@ -37,4 +37,23 @@ RSpec.describe ProdutosController do
             expect(produto).not_to be_valid
         end
     end
+    describe 'deve ser cadastrado corretamente por um usuário logado' do
+      before { 
+        @usuario = Usuario.create!(
+          email:"teste@teste.com", 
+          password: pass, 
+          password_confirmation: pass, 
+          created_at: date, 
+          updated_at: date
+        )
+        session[:usuario_id] = @usuario.id
+      }
+      it 'em caso de cadastro correto podemos editar' do
+        @produto = Produto.create(nome:"Camiseta", situacao:"Nova", categoria:"Adulto", tamanho:"GG", imagem:"teste.jpg", usuario_id:@usuario.id)
+        expect do
+          patch :update, params: { nome: "Blusa", situacao: "Usada", categoria:"Adulto", tamanho:"GG", imagem:"teste.jpg", usuario_id:@usuario.id }
+          expect(@produto.update params).to change(@produto) 
+        end          
+      end
+    end
 end
