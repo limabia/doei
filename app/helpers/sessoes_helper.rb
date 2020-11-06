7af7c0f3200f8c5bf4caf6b1b386f9ff6281638d
Bianca Lima <bianca.lima.1504@gmail.com>
2020-11-05 23:27:56 -0300

    refatoração - remoção de função nao utilizada

--- app/helpers/sessoes_helper.rb
+++ app/helpers/sessoes_helper.rb
@@ -6,9 +6,4 @@ module SessoesHelper
     def entrar_como(usuario)
         "logado #{usuario}"
     end
-
-    # Returns true if the user is logged in, false otherwise.
-    def logged_in?
-        !current_user.nil?
-    end
 end
