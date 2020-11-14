57dd6f415e3c4ea8fea305787c74a90aa830a555
Mauricio Tavares Ozaki <ozaki@versatushpc.com.br>
2020-11-14 00:46:10 -0300

    Função que altera o status "Já doado" do produto de "Não" para "Sim".

--- features/step_definitions/produtos_cadastrar_step.rb
+++ features/step_definitions/produtos_cadastrar_step.rb
@@ -36,3 +36,6 @@ end
 Então('não deverei ver a {string} do produto') do |string|
     expect(page).to have_content(string)
 end
+Quando('seleciono Sim') do
+    find(:xpath, '//option[contains(text(), "Sim")]').select_option
+end
